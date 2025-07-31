7e0ec966acce1f76d69b67d9afe2a7bd4ac23b06
N1ck <cbnickp@gmail.com>
2025-07-31 17:04:15 +0700

    Update page.js

--- src/app/[lang]/journey/page.js
+++ src/app/[lang]/journey/page.js
@@ -9,32 +9,28 @@ export default function Page() {
     {
       year: "2020",
       events: [
-        "EZ Brothers Genetics worked on diesel crosses",
-        "Presented entire line including Sour Krunchz (Karma's Sour Diesel x Runtz)",
+        "EZ Brothers Genetics was working on the diesel crossed and generously presented us with the whole line including the Sour Krunchz (Karma's Sour Diesel x Runtz)",
+        "While Phet Manil gifted us with the Stardawg Guava Wookie from Bodhi Seeds",
       ],
     },
     {
       year: "2021",
       events: [
-        "Sour Krunchz flowers and Temple Ball dropped exclusively at Sukhumweed Industries",
-        "Under Ras (HSO) guidance, selected and managed Ghost Candy male (Geist Grow)",
-        "Two crosses were made from Ghost Candy male, unnamed until 2023",
+        "Flowers and Temple Ball of the Sour Krunchz exclusively drop at Sukhumweed industries. In the same years, with a guidance from Ras (HSO), we were able to select and manage the male of Ghost Candy (Geist Grow) properly. Two crossed was made from this Ghost Candy male. both stay unnamed until 2023",
       ],
     },
     {
       year: "2022",
       events: [
-        'Tested both crosses at "Undisclosed University" with Robert_Motorcycle and PowerPuff from Shovel',
-        'Sour Krunchz x Ghost Candy labeled as "s" and "M"',
+        `Testing both crosses at "Undisclosed University" with Robert_Motorcycle and PowerPuff from Shovel.`,
+        `Sour Krunchz x Ghost Candy tagged as “S” and stardawg guava wookie X GHOST CANDY tagged as “M”`
       ],
     },
     {
       year: "2023",
       events: [
-        "Left the university, continued pheno-hunting",
-        "Found s#2 and s#21 as keeper and family favorite",
-        'Named “s” as Robert\'s Mint to honor Robert_Motorcycle and mint traits',
-        'Named “M” as Samwise Rise',
+        "No longer with the university but pheno-hunting continue, found S#2, S#21 and M#16 as a keeper and family favorite.",
+        "“S” was finally named as ROBERT'S MINT to honor Robert_Motorcycle hard work at the undisclosed university and the cultivar mint character, while “M” named as SAMWISE RISE."
       ],
     },
   ];
@@ -46,7 +42,7 @@ export default function Page() {
             {timeline.map(({ year, events }) => (
               <li key={year}>
                 <h3 className="text-xl font-semibold mb-4">{year}</h3>
-                <ul className="ml-6 space-y-1">
+                <ul className="list-disc ml-6 space-y-1">
                   {events.map((event, idx) => (
                     <li key={idx}>{event}</li>
                   ))}
